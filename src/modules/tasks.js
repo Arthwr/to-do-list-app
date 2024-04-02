7d00b18c38a1f251b2d3715a96d23712b1dae5a5
arthur <artyr007@gmail.com>
2024-04-02 20:46:39 +0500

    Abstract id generation function from Task class & set generateTaskID function as default

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,8 +1,9 @@
 import generateTaskID from "./idGenerator";
 
+// prettier-ignore
 export default class Task {
-  constructor(title, description, dueDate, priority) {
-    this.id = generateTaskID();
+  constructor(title, description, dueDate, priority, idGenerator = generateTaskID) {
+    this.id = idGenerator();
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
